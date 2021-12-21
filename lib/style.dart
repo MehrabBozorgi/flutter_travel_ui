40ebb8600ccb4f9f6a991ea68d8561be0d73713c
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-12-22 01:26:40 +0330

    flutter_travel_ui

---
+++ lib/style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const bigBlackTitleStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 30);
+//
+const bigGreyTitleStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 22);
+//
+const titleStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 18);
+//
+const greyStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 16);
+//
+const greyDescStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 14);
+//
+const blackStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 16);
+//
+const blackStyle2=TextStyle(color: Colors.black,fontSize: 16);
+//
+const blackTitleStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 18);
+//
+const whiteStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 16);
+//
+const whiteTitleStyle=TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 18);
